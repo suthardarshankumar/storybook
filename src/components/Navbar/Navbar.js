4496308bba2895722068a80dd25805c8af195b8a
suthardarshankumar <suthardarshan2782@gmail.com>
2024-04-16 17:41:31 +0530

    Complete Navbar.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,44 +11,47 @@ const Navbar = () => {
     return (
         <>
             <nav className={styles.navbar}>
-                <div className={styles.innerNavbar}>
-                    <div className={styles.logoSection}>
-                        <img src={logo} alt='' className={styles.logo} />
-                        <h1 className={styles.logoHeading}>Storybook</h1>
+                <div className={styles.mainNavbar}>
+                    <div className={styles.innerNavbar}>
+                        <div className={styles.logoSection}>
+                            <img src={logo} alt='' className={styles.logo} />
+                            <h1 className={styles.logoHeading}>Storybook</h1>
+                        </div>
+                        <div className={styles.links}>
+                            Docs
+                        </div>
+                        <div className={styles.links}>
+                            Addons
+                        </div>
+                        <div className={styles.links}>
+                            Showcase
+                        </div>
+                        <div className={styles.links}>
+                            Blog
+                        </div>
+                        <div className={styles.links}>
+                            Visual Test<GoArrowUpRight className={styles.arrowIcon} />
+                        </div>
+                        <div className={styles.links}>
+                            Enterprise<GoArrowUpRight className={styles.arrowIcon} />
+                        </div>
                     </div>
-                    <div className={styles.links}>
-                        Docs
-                    </div>
-                    <div className={styles.links}>
-                        Addons
-                    </div>
-                    <div className={styles.links}>
-                        Showcase
-                    </div>
-                    <div className={styles.links}>
-                        Blog
-                    </div>
-                    <div className={styles.links}>
-                        Visual Test<GoArrowUpRight className={styles.arrowIcon} />
-                    </div>
-                    <div className={styles.links}>
-                        Enterprise<GoArrowUpRight className={styles.arrowIcon} />
+                    <div className={styles.rightNavbar}>
+                        <div className={styles.otherLink}>
+                            <FaGithub className={styles.githubIcon} />
+                            <span className={styles.githubNum}>82,706</span>
+                        </div>
+                        <div className={styles.otherLink}>
+                            <CiSearch className={styles.searchIcon} />
+                            <span className={styles.search}>Search docs</span>
+                            <span className={styles.iconContain}>
+                                <MdKeyboardCommandKey className={styles.commandIcon} />
+                                <TbLetterK className={styles.KIcon} />
+                            </span>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.rightNavbar}>
-                    <div className={styles.otherLink}>
-                        <FaGithub className={styles.githubIcon} />
-                        <span className={styles.githubNum}>82,706</span>
-                    </div>
-                    <div className={styles.otherLink}>
-                        <CiSearch className={styles.searchIcon} />
-                        <span className={styles.search}>Search docs</span>
-                        <span className={styles.iconContain}>
-                            <MdKeyboardCommandKey className={styles.commandIcon} />
-                            <TbLetterK className={styles.KIcon} />
-                        </span>
-                    </div>
-                </div>
+                <div className={styles.square}></div>
             </nav>
         </>
     )
